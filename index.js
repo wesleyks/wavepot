24b55eeb90e1cf14d30063a1dbfc519f5709c8ac
wesleyks <wesleyksun@gmail.com>
2015-04-21 19:04:22 -0700

    eh

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 /**
  * @name wavepot
  */
-
+var TAU = 2.0 * Math.PI;
+function sin(t, f) {
+  return Math.sin(t * f * TAU);
+}
+function square(t, f) {
+  return Math.sign(Math.sin(t * f * TAU));
+}
+function saw(t, f) {
+  var period = 1.0 / f;
+  return 2.0 * ((t / period) - Math.floor((1.0 / 2.0) + (t / period)));
+}
 export function dsp(t) {
-  return 0;
+  return saw(t, 440) * sin(t, 440);
 }
